d26a3bb6e119d018fcb1a5628ffbc61a8ef71bef
Kevin Tang <103300322+XinKai-Tang@users.noreply.github.com>
2023-10-13 13:13:36 +0800

    Create __init__.py

---
+++ nets/__init__.py
@@ -0,0 +1,7 @@
+# from .unetr.unetr import UNETR
+# from .swin_unetr.swin_unetr import SwinUNETR
+# from .upernext.upernext import UPerNeXt
+# from .unetr_pp.unetr_pp import UNETR_PP
+# from .uxnet3d.uxnet3d import UXNET
+
+from .conv_unet.conv_unet import ConvUNET
